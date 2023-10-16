95ac9718be50934bdfd3e18892aa529bf373c564
samruddhi <30900933+samruddhijp05@users.noreply.github.com>
2023-10-16 10:02:00 -0400

    Update query4.sql

--- sqlquery4/query4.sql
+++ sqlquery4/query4.sql
@@ -1,6 +1,5 @@
-SELECT customer_id , count(*) AS Rental_Count
+SELECT customer_id , count(*) AS total_rental
 FROM rental
-WHERE rental_date BETWEEN '2005-07-01' AND '2005-08-31' 
 GROUP BY customer_id 
 ORDER BY Rental_Count DESC;
 
