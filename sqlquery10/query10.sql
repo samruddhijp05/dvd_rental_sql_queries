457a2014477838a1dddf1982fae968b25b1c3b8d
samruddhi <30900933+samruddhijp05@users.noreply.github.com>
2023-10-16 10:40:07 -0400

    Update query10.sql

--- sqlquery10/query10.sql
+++ sqlquery10/query10.sql
@@ -1,4 +1,12 @@
-SELECT film.title, category.name 
-FROM film 
-CROSS JOIN category 
-LIMIT 100;
+SELECT country, count(DISTINCT customer_id) AS customer_base, SUM(amount) AS total_sales
+FROM country
+JOIN city
+USING(country_id)
+JOIN address
+USING(city_id)
+JOIN customer
+USING (address_id)
+JOIN payment
+USING(customer_id)
+GROUP BY 1
+ORDER BY 2 DESC;
