7116e503ea3a0807026fdc336df512ab95e08bd8
samruddhi <30900933+samruddhijp05@users.noreply.github.com>
2023-10-15 23:41:56 -0400

    Add files via upload

--- sqlquery6/query6.sql
+++ sqlquery6/query6.sql
@@ -1,1 +1,8 @@
-SELECT NOW();
+SELECT rating, 
+       AVG(length) AS avg_length, 
+       CASE 
+         WHEN AVG(length) > 120 THEN 'Long'
+         ELSE 'Short'
+       END AS film_length_category
+FROM film 
+GROUP BY rating;
