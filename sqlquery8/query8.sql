d6d00c3bdd8727b551783e0cf63c2ffb93c60d60
samruddhi <30900933+samruddhijp05@users.noreply.github.com>
2023-10-16 10:20:09 -0400

    Update query8.sql

--- sqlquery8/query8.sql
+++ sqlquery8/query8.sql
@@ -1,3 +1,14 @@
-SELECT MIN(length) as min_length, MAX(length) as max_length 
-FROM film 
-WHERE title LIKE 'A%';
+SELECT c.name AS Genre, count(DISTINCT cu.customer_id) AS Total_rent_demand
+FROM category c
+JOIN film_category fc
+USING(category_id)
+JOIN film f
+USING(film_id)
+JOIN inventory i
+USING(film_id)
+JOIN rental r
+USING(inventory_id)
+JOIN customer cu
+USING(customer_id)
+GROUP BY 1
+ORDER BY 2 DESC;
