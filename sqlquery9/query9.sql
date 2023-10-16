1459ee36fab3d05babea434751ca80143d9ad632
samruddhi <30900933+samruddhijp05@users.noreply.github.com>
2023-10-15 23:43:30 -0400

    updated query 9

--- sqlquery9/query9.sql
+++ sqlquery9/query9.sql
@@ -1,1 +1,4 @@
-SELECT NOW();
+SELECT customer.customer_id, customer.first_name, customer.last_name, rental.rental_id 
+FROM customer 
+RIGHT JOIN rental ON customer.customer_id = rental.customer_id 
+ORDER BY rental.rental_date DESC;
