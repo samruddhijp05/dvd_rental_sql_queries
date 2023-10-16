50f5427e910efb5d36b2fd44c5429553ee2aadc1
samruddhi <30900933+samruddhijp05@users.noreply.github.com>
2023-10-16 10:25:15 -0400

    Update query6.sql

--- sqlquery6/query6.sql
+++ sqlquery6/query6.sql
@@ -1,8 +1,8 @@
-SELECT rating, 
-       AVG(length) AS avg_length, 
-       CASE 
-         WHEN AVG(length) > 120 THEN 'Long'
-         ELSE 'Short'
-       END AS film_length_category
-FROM film 
-GROUP BY rating;
+SELECT c.name AS genre, ROUND(AVG(f.rental_rate),2) AS Average_rental_rate
+FROM category c
+JOIN film_category fc
+USING(category_id)
+JOIN film f
+USING(film_id)
+GROUP BY 1
+ORDER BY 2 DESC;
