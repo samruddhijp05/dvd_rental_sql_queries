92697ffd43c1ed4ef83c4da6a475fa2e6ff7734f
samruddhi <30900933+samruddhijp05@users.noreply.github.com>
2023-10-16 09:58:56 -0400

    Update query2.sql

--- sqlquery2/query2.sql
+++ sqlquery2/query2.sql
@@ -1,5 +1,5 @@
-SELECT film.title, COUNT(actor.actor_id) as actor_count
+SELECT concat(actor.first_name,' ', actor.last_name) as actor_name, COUNT(film.film_id) as film_count
 FROM film
 INNER JOIN film_actor ON film.film_id = film_actor.film_id
 INNER JOIN actor ON actor.actor_id = film_actor.actor_id
-GROUP BY film.title;
+GROUP BY actor_name;
