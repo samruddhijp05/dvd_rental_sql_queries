ed4e6544febc216c61b1cf5b3edfd6007130e7f0
samruddhi <30900933+samruddhijp05@users.noreply.github.com>
2023-10-16 10:27:18 -0400

    Update query5.sql

--- sqlquery5/query5.sql
+++ sqlquery5/query5.sql
@@ -1,3 +1,10 @@
-SELECT DISTINCT customer_id 
-FROM rental 
-WHERE customer_id IN (SELECT customer_id FROM payment WHERE amount > 10);
+SELECT DISTINCT category_name,
+	COUNT(film_title) OVER(PARTITION BY  category_name) AS category_count
+
+FROM
+	(SELECT f.title film_title, c.name category_name
+	FROM film f 
+	JOIN film_category fc ON fc.film_id = f.film_id
+	JOIN category c ON c.category_id = fc.category_id) t1
+
+ORDER BY category_count;
